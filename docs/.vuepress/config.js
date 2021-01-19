d271358c64fc5eec0609bc9b91fcf2a6488b0e38
chuangli <chuangli@sjtu.edu.cn>
2021-01-19 19:31:59 +0800

    finish

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,24 @@
 module.exports = {
     title: "李闯的博客",
-    description: '李闯的博客'
-  }
+    description: '李闯的博客',
+    themeConfig: {
+      nav: [
+        { text: '主页', link: '/' },                      // 根路径
+        { text: '页面', link: '/pages/' },
+        { text: '我的大学', link: 'https://www.sjtu.edu.cn' }, // 外部链接
+
+        // 下拉列表显示分组
+        {
+          text: '高级页面',
+          items: [
+            { 
+              text: '页面', 
+              items: [
+                { text: '1', link: '/pages/1' },
+                { text: '2', link: '/pages/2' }
+              ] 
+            }]
+        }
+      ]
+    }
+}
