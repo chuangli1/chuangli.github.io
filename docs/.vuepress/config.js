77f217fb9da39be85adfd1ab184e572975c7d5b6
chuangli <43231436+chuangli1@users.noreply.github.com>
2021-02-02 00:27:55 +0800

    add siderbar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,5 +3,21 @@ module.exports = {
     description: '明天，你好',
     base:'/',
     themeConfig: {
+        search: true,
+        searchMaxSuggestions: 10,
+        searchPlaceholder: 'Search...',
+        sidebar: {
+            '/experience/autocell/': [
+              '0',     /* /foo/ */
+              '1',  /* /foo/one.html */
+              '2'   /* /foo/two.html */
+            ],
+            '/experience/epm2/': [
+                '0',     /* /foo/ */
+                '1',  /* /foo/one.html */
+                '2'   /* /foo/two.html */
+              ],
+           
+          }
     }
 }
--- docs/experience/README.md
+++ docs/experience/README.md
@@ -1,1 +1,9 @@
-# 这是我的项目经历
+---
+
+---
+[Autocell-a system for culturing cells on web](/experience/autocell/0)</br>
+
+[ERP企业实习,一段美好的实习时光](/experience/epm2/0)</br>
+
+[软件部分](/aboutme)</br>
+
