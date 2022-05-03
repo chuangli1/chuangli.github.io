234c38a82fb16fc83d2c3c8f559431abcb48d374
chuangli <43231436+chuangli1@users.noreply.github.com>
2022-05-03 20:50:55 +0800

    'finish1'

---
+++ news.ts
@@ -0,0 +1,23 @@
+const newsList:any ={
+    '0':[
+        {
+            title: '新闻，微纳微纳，十',
+            url: 'https://www.runoob.com/cssref/func-calc.html'
+        },
+        {
+            title: '新闻，微纳微纳，十大科技打开即可',
+            url: 'https://www.baidu.com'
+        },
+        {
+            title: '新闻，微纳微纳，十大科技打开即可',
+            url: 'https://www.baidu.com'
+        },
+        {
+            title: '新闻，微纳微纳，十大科技打开即可',
+            url: 'https://www.baidu.com'
+        }
+    
+    ]
+
+};
+export default newsList;
