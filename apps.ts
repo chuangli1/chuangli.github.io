e400fdf59a43fab9856aadff499ea89cf8d615e6
chuangli <43231436+chuangli1@users.noreply.github.com>
2022-05-04 14:48:48 +0800

    'finish'

---
+++ apps.ts
@@ -0,0 +1,98 @@
+const apps:any = {
+    'vue':[
+        {
+            name:'Vue',
+            icon:'https://v3.cn.vuejs.org/logo.png',
+            url:'https://v3.cn.vuejs.org/guide/introduction.html'
+        },
+        {
+            name:'Vue List',
+            icon:'https://v3.cn.vuejs.org/logo.png',
+            url:'https://github.com/vuejs/awesome-vue'
+        },
+        {
+            name:'VueX',
+            icon:'https://v3.cn.vuejs.org/logo.png',
+            url:'https://vuex.vuejs.org/zh/index.html'
+        },
+        {
+            name:'VueRouter',
+            icon:'https://v3.cn.vuejs.org/logo.png',
+            url:'https://router.vuejs.org/zh/introduction.html'
+        },
+        {
+            name:'Vite',
+            icon:'https://vitejs.cn/logo.svg',
+            hover:'下一代前端开发与构建工具',
+            url:'https://vitejs.cn/guide/'
+        },
+        {
+            name:'Vitest',
+            icon:'https://cn.vitest.dev/logo.svg',
+            hover:'由 Vite ⚡️提供支持的极速单元测试框架',
+            url:'https://cn.vitest.dev/'
+        },
+        {
+            name: 'less',
+            url:'https://less.bootcss.com/',
+            icon: 'https://less.bootcss.com/public/img/less_logo.png'
+        },
+        {
+            name: 'D3',
+            url: 'https://github.com/d3/d3/wiki',
+            icon: 'https://camo.githubusercontent.com/586ccf0aad9684e…8747470733a2f2f64336a732e6f72672f6c6f676f2e737667'
+        },
+        {
+            name:'Three',
+            icon: 'https://bestofjs.org/logos/threejs.svg',
+            url: 'https://threejs.org/'
+        }
+    ],
+    'lib':[
+        {   
+            name:'Element(组)',
+            icon:'https://element-plus.gitee.io/images/element-plus-logo.svg',
+            url:'https://element-plus.gitee.io'
+        },
+        {   
+            name:'Vant(组)',
+            icon:'https://cdn.jsdelivr.net/npm/@vant/assets/logo.png',
+            url:'https://vant-contrib.gitee.io/vant/#/zh-CN'
+        },
+        {   
+            name:'Vuetify(组)',
+            url:'https://vuetifyjs.com/zh-Hans/introduction/why-vuetify/#feature-guides'
+        },
+        {
+            name:'IconPark(图)',
+            icon: 'https://lf1-cdn2-tos.bytegoofy.com/bydesign/iconparksite/static/media/logo_with_name.598fc011.svg',
+            url: 'https://iconpark.oceanengine.com/home'
+        },
+        {
+            name: 'UI5(组)',
+            url: 'https://sap.github.io/ui5-webcomponents/playground/getting-started',
+            icon: 'https://sap.github.io/ui5-webcomponents/assets/images/ui5-logo.png'
+        },
+        {   
+            name: 'ag-grid',
+            url: 'https://www.ag-grid.com/documentation/',
+            icon: 'data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PSIwIDAg…sPSIjZmZmIiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48L3N2Zz4='
+        },
+        {
+            name: 'HighChart',
+            icon: 'https://s1.jscdn.com.cn/highcharts/images/logo.svg',
+            url: 'https://www.highcharts.com.cn/docs/start-introduction'
+        },
+        {
+            name: 'FormKit',
+            url: 'https://formkit.com/',
+            icon: ''
+        },
+        {
+            name: 'Bootstrap',
+            url: 'https://v5.bootcss.com/',
+            icon: 'https://getbootstrap.com/docs/5.1/assets/img/bootstrap-icons@2x.png'
+        }
+    ]
+}
+export default apps;
--- news.ts
+++ news.ts
@@ -1,22 +1,82 @@
 const newsList:any ={
     '0':[
         {
-            title: '新闻，微纳微纳，十',
-            url: 'https://www.runoob.com/cssref/func-calc.html'
+            title: '阮一峰的网络日志',
+            url: 'https://www.ruanyifeng.com/blog/'
         },
         {
-            title: '新闻，微纳微纳，十大科技打开即可',
-            url: 'https://www.baidu.com'
+            title: '前端知识体系大全',
+            url: 'http://caibaojian.com/toutiao/7186/'
         },
         {
-            title: '新闻，微纳微纳，十大科技打开即可',
-            url: 'https://www.baidu.com'
+            title: '前端面试之道',
+            url: 'http://caibaojian.com/interview-map/frontend/'
         },
         {
-            title: '新闻，微纳微纳，十大科技打开即可',
-            url: 'https://www.baidu.com'
+            title: '计算机教育中缺失的一课',
+            url: 'https://missing-semester-cn.github.io/'
+        },
+        {
+            title: '前端开发文档',
+            url:'https://wangdoc.com/'
+        },
+        {
+            title: '前端博客大全',
+            url:'http://caibaojian.com/links/'
         }
     
+    ],
+    '1':[
+        {
+            title: '你是如何学会正则表达式的？',
+            url: 'https://www.zhihu.com/question/48219401/answer/742444326',
+            new: true
+        },
+        {
+            title: '面试官，别问我 HTTP 了！看这 30 张图就行！',
+            url: 'https://mp.weixin.qq.com/s/4epUWMCLpwCBxxQgEbL8gA',
+            new: true
+        } 
+    ],
+    '2':[
+        {
+            title: '红楼梦(解说)',
+            url: 'https://www.bilibili.com/video/BV1CC4y1a7ee?spm_id_from=333.337.search-card.all.click'
+        },
+        {
+            title: '大明王朝1566(解说)',
+            url: 'https://www.bilibili.com/medialist/play/669154966?from=space&business=space_collection&business_id=35282&desc=0'
+        },
+        {
+            title: '激荡四十年(视频)',
+            url: 'https://www.bilibili.com/video/BV1AK4y147uB/?spm_id_from=333.788'
+        },
+        {
+            title: '贫嘴张大民的幸福生活(解说)',
+            url: 'https://www.bilibili.com/video/BV1ER4y177rS?spm_id_from=333.999.0.0'
+        },
+        {
+            title: '绝命毒师(解说)',
+            url: 'https://www.bilibili.com/video/BV1b44y1g7UD?spm_id_from=333.337.search-card.all.click'
+        },
+        {
+            title: '必应图片(图片)',
+            url: 'https://bing.wilii.cn/index.html',
+            new: true
+        },
+        {
+            title: '灿烂人生(电影解说)',
+            url: 'https://www.bilibili.com/video/BV1iJ411w7YU?spm_id_from=333.337.search-card.all.click'
+        },
+        {
+            title: '生活多美好(电影解说)',
+            url: 'https://www.bilibili.com/video/BV1LZ4y1s7HD?spm_id_from=333.337.search-card.all.click'
+        },
+        {
+            title: '白色巨塔(解说)',
+            url: 'https://www.bilibili.com/video/BV1c64y1k7cd?spm_id_from=333.999.0.0'
+        }
+
     ]
 
 };
