3ff57de4ff5fa1b448deb70b90a643583ac58cbf
chuangli <43231436+chuangli1@users.noreply.github.com>
2022-07-31 21:56:39 +0800

    'deploy'

--- assets/ppt.10cffc00.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.43837204.js";import{o as t,a as r}from"./vendor.047185e5.js";const _={},o={src:"https://pptist.gitee.io/",class:"ppt",frameborder:"0"};function s(a,p){return t(),r("iframe",o," ")}var i=e(_,[["render",s]]);export{i as default};
---
+++ assets/ppt.ab338af3.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.83d53154.js";import{o as t,a as r}from"./vendor.047185e5.js";const _={},o={src:"https://pptist.gitee.io/",class:"ppt",frameborder:"0"};function s(a,p){return t(),r("iframe",o," ")}var i=e(_,[["render",s]]);export{i as default};
